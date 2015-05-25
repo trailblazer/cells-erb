661d1ad0efe7d290ad7d336d7bc818c3762ede1a
Nick Sutterer <apotonick@gmail.com>
2015-05-25 15:11:28 +1000

    incredible how much bullshit one can add to run 3 test cases hahahaha :grimace:

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,4 @@
 require "pp"
-
-begin
-  require 'byebug'
-rescue LoadError
-end
-require 'minitest/reporters'
-Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new]
 require 'minitest/autorun'
 
 ENV['RAILS_ENV'] = 'test'

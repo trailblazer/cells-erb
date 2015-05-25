7e1d998efd40aa8e65114ab986cd2e2bfb845a5c
Nick Sutterer <apotonick@gmail.com>
2015-05-25 14:39:58 +1000

    we don't need outbuf anymore, it's output_buffer everywhere, y'all!

--- lib/cell/erb.rb
+++ lib/cell/erb.rb
@@ -3,12 +3,8 @@ require "erbse"
 module Cell::Erb
   # Erbse-Tilt binding. This should be bundled with tilt. # 1.4. OR should be tilt-erbse.
   class Template < Tilt::Template
-    def self.default_output_variable
-      "_erbout"
-    end
-
     def self.engine_initialized?
-      defined? ::Erbse::Eruby
+      defined? ::Erbse::Template
     end
 
     def initialize_engine
@@ -19,17 +15,13 @@ module Cell::Erb
     #
     #   Tilt.new("#{base}/#{prefix}/#{view}", engine_class: Erbse::Eruby)
     def prepare
-      @outvar = options.delete(:outvar) || self.class.default_output_variable
-
-      @options.merge!(:bufvar => @outvar)
-
       engine_class = options.delete(:engine_class)
       # engine_class = ::Erubis::EscapedEruby if options.delete(:escape_html)
-      @engine = (engine_class || ::Erbse::Eruby).new(data, options)
+      @template = (engine_class || ::Erbse::Template).new(data, options)
     end
 
     def precompiled_template(locals)
-      @engine.src
+      @template.call
     end
   end
 end

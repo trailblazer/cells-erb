3eaa115fdf64a753b04dc91de5b8303e1fc7f795
Nick Sutterer <apotonick@gmail.com>
2016-10-04 15:39:13 +0800

    remove all output buffer/capture code, this is now simply done via yield, as it should be. WARNING: #concat is still broken.

--- lib/cell/erb/template.rb
+++ lib/cell/erb/template.rb
@@ -3,15 +3,6 @@ require "erbse"
 module Cell
   # Erb contains helpers that are messed up in Rails and do escaping.
   module Erb
-    # This is to prevent multiple renders to share the same output_buffer.
-    # I am still trying to find a way to avoid output buffers.
-    def render_template(*)
-      old_output_buffer = @output_buffer
-      super
-    ensure
-      @output_buffer = old_output_buffer
-    end
-
     def template_options_for(options)
       {
         template_class: ::Cell::Erb::Template,
@@ -21,19 +12,7 @@ module Cell
 
     # this is capture copied from AV:::CaptureHelper without doing escaping.
     def capture(*args)
-      value = nil
-      buffer = with_output_buffer { value = yield(*args) }
-
-      return buffer.to_s if buffer.size > 0
-      value # this applies for "Beachparty" string-only statements.
-    end
-
-    def with_output_buffer(block_buffer=ViewModel::OutputBuffer.new)
-      @output_buffer, old_buffer = block_buffer, @output_buffer
-      yield
-      @output_buffer = old_buffer
-
-      block_buffer
+      yield(*args)
     end
 
     # Below:
